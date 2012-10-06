81f59677abc4b3a8ff111b4f29aa302cc9738350
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2012-10-06 17:00:23 +0200

    Added --cell_char and --geom options

--- main.py
+++ main.py
@@ -20,20 +20,25 @@ except ImportError:
 
 class CellScreen():
 
-    def __init__(self):
+    def __init__(self, size_x=50, size_y=20):
+        self._cell_sign = "#"
         self.screen = curses.initscr()
-        self.size_x, self.size_y = self.screen.getmaxyx()
-#        self.size_x, self.size_y = 20,20
-        self.size_x, self.size_y = self.size_x-1, self.size_y-1
+        if size_x == "MAX" or size_y == "MAX":
+            self.size_x, self.size_y = self.screen.getmaxyx()
+            self.size_x, self.size_y = self.size_x-1, self.size_y-1
+        else:
+            self.size_x, self.size_y = size_x, size_y
        # self.screen.border(0)
-        self.screen.refresh()
         signal.signal(signal.SIGINT, self._sig_hdl)
         self._init_board()
+
+    def set_cell(self, chrnum):
+        self._cell_sign = chrnum
 
     def draw_board(self):
         for cell in self.board:
             #if self.board[cell] == 1: self.screen.addstr(cell[0], cell[1], str(chr(0x25a0)))
-            if self.board[cell] == 1: self.screen.addstr(cell[0], cell[1], "O")
+            if self.board[cell] == 1: self.screen.addstr(cell[0], cell[1], self._cell_sign)
         self.screen.refresh()
         self.screen.clear()
 
@@ -77,14 +82,21 @@ class CellScreen():
 def main():
     parser = argparse.ArgumentParser(description="%s by %s" % (__appname__, __author__))
     parser.add_argument("-d", "--delay", help="Delay for drawing, default 1s", action="store", dest="delay", type=int, default=1)
+    parser.add_argument("-g", "--geometry", help="Geometry e.g. 50x50 or MAX for maximal", action="store", dest="geom", type=str, default="MAX")
+    parser.add_argument("--cell_char", help="Geometry e.g. 50x50 or MAX for maximal", action="store", dest="cell_char", type=str, default="#")
     args = parser.parse_args()
-    cs = CellScreen()
+    cs = None
+    if args.geom == "MAX":
+        cs = CellScreen(args.geom)
+        cs.set_cell(args.cell_char)
+    else:
+        geom = args.geom.split("x")
+        cs = CellScreen(int(geom[0]), int(geom[1]))
+        cs.set_cell(args.cell_char)
     while True:
         cs.update_board()
         cs.draw_board()
         time.sleep(args.delay)
-
-
 
 if __name__ ==  "__main__":
     main()
